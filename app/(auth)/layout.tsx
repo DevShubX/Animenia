345f98b14722d2d511e34fae69010fd43c35eb89
Mugdha <mugdha6123@gmail.com>
2024-10-03 12:49:21 +0530

    Created Register Page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const AuthLayout = ({children}:{children:React.ReactNode}) => {
   return (
     <div className='w-full h-full relative'>
-      <Image className="w-full h-full" src="/static/Background.jpg" alt="bg" width={1920} height={1080}  />
+      <Image className="w-full h-full object-cover" src="/static/Background.jpg" alt="bg" width={1920} height={1080}  />
       <main className='absolute top-0 left-0 w-full h-full'>
         <div className='flex justify-center items-center w-full h-full'>
           {children}
