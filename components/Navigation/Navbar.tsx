d939b01ba6b4f3f5b99a3b0d250a635be03f4e57
Mugdha <132761231+JIYA1MU@users.noreply.github.com>
2024-10-05 20:42:41 +0530

    Added navbar

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
+
   return (
-    <div>Navbar</div>
+    <nav className='bg-white flex items-center w-full h-full justify-between p-2'>
+      <div>
+        <Image className='w-[8rem] h-[2rem]' src="/static/Animenia 1.png" alt ="logo" width={100} height={100}/>
+      </div>
+      <div className="hidden md:flex space-x-6">
+        <Link href="#" className="hover:text-gray-400">All</Link>
+        <Link href="#" className="hover:text-gray-400">Action</Link>
+        <Link href="#" className="hover:text-gray-400">Genre</Link>
+      </div>
+      <div>
+
+      </div>
+    </nav>
   )
 }
 
